e728a0f8b42c10f8537c5dd3098286ddfb9563f3
fforce <francolqr@gmail.com>
2018-07-05 22:38:09 -0300

    componentes

---
+++ proyecto/src/Componentes/MovieApp/MovieApp.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const MovieApp = ({name}) => (
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">React Movie DB APP</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsExample07">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="peliculas-grid.html">Peliculas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="series-grid.html">Series</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="mi-lista-grid.html">Mi Lista <span class="badge badge-danger">3</span></a>
+                    </li>
+                </ul>
+                <div class="form-inline my-2 my-md-0" action="busqueda-peliculas-grid.html">
+                    <input class="form-control" type="text" placeholder="Buscar Película o Serie" aria-label="Search" />
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
+);
+
+export default MovieApp;
--- proyecto/src/index.js
+++ proyecto/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Componentes/MovieApp';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
