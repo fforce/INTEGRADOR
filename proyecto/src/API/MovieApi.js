339fbf642a4a8a4829d6d7e145494fa3bfe834b2
fforce <francolqr@gmail.com>
2018-08-05 22:13:43 -0300

    d

--- proyecto/src/API/MovieApi.js
+++ proyecto/src/API/MovieApi.js
@@ -8,20 +8,19 @@ class MovieApi {
         this.axios = axios.create({
             baseURL: 'https://api.themoviedb.org/3/',
             params: {
+                api_key :api_key,
                 headers: { "Access-Control-Allow-Origin": "*" },
             },
         })
     }
 
-
     getMoviesPopulars = () => {
-        return this.axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`).then(response => {
+        return this.axios.get(`movie/popular?language=en-US&page=1`).then(response => {
             const comics = response.data.results.map((item) => (
                 {
                     title: item.title,
                     date: item.release_date,
-                    imgUrl: imgPath + item.poster_path,
-               
+                    imgUrl: imgPath + item.poster_path,               
                 }
             ))
             debugger
