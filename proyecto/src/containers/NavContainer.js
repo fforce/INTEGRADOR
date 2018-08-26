5b9a95a8fda9adaeba4e8b749e610eacb3bea53c
fforce <francolqr@gmail.com>
2018-08-26 19:14:12 -0300

    xx

--- proyecto/src/containers/NavContainer.js
+++ proyecto/src/containers/NavContainer.js
@@ -6,8 +6,6 @@ import Nav from '../components/Nav'
 class NavContainer extends Component {   
 
     render() {
-        const { peliculas, showMoviesListItems } = this.props;
-
         return (
             <Nav counter={this.props.counter} ></Nav>
         )
